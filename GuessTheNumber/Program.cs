f60e00f9b8ffff6d35ed0354282a12d11d408acb
anastasia-lab <anastasiya.shpilevaia@ictis.sfedu.ru>
2022-03-20 19:30:14 +0300

    Добавьте файлы проекта.

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в игру \"Угадай-ка\".");
+            Console.WriteLine("Необходимо угадать число, которое загадал компьютер. Но для начала...\n");
+            Console.Write("Введите максимальное целое число диапазона: ");
+            int RangeNumber = int.Parse(Console.ReadLine());
+
+            Random rnd = new Random();
+            int RandomNumberForGame = rnd.Next(0, 10);
+            int count = 0;
+
+            while (true)
+            {
+                count++;
+
+                Console.Write("\nВведите загаданное число: ");
+                string UserString = Console.ReadLine();
+                if (UserString == "")
+                {
+                    Console.WriteLine("\nОчень жаль, что вы уходите.");
+                    Console.WriteLine($"Загаданное число: {RandomNumberForGame}");
+                    break;
+                }
+
+                int UserNumber = int.Parse(UserString);
+
+                if (UserNumber < RandomNumberForGame)
+                {
+                    Console.WriteLine($"Число {UserNumber} меньше загаданного\n");
+                }
+
+                else if (UserNumber > RandomNumberForGame)
+                {
+                    Console.WriteLine($"Число {UserNumber} больше загаданного\n");
+                }
+
+                else if (UserNumber == RandomNumberForGame)
+                {
+                    Console.WriteLine($"\nПоздравляю! Вы угадали число. Число попыток: {count}");
+                    break;
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
