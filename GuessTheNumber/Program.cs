aa038151328afc04c45e627b1bdc08ef9c61d284
anastasia-lab <anastasiya.shpilevaia@ictis.sfedu.ru>
2022-03-22 21:16:36 +0300

    modification

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -15,41 +15,51 @@ namespace GuessTheNumber
             Console.Write("Введите максимальное целое число диапазона: ");
             int RangeNumber = int.Parse(Console.ReadLine());
 
-            Random rnd = new Random();
-            int RandomNumberForGame = rnd.Next(0, RangeNumber);
-            int count = 0;
-
-            while (true)
+            if (RangeNumber <= 0)
             {
-                count++;
+                Console.WriteLine("\nУпс.. Кажется вы ввели неправильное значение.");
+                Console.WriteLine("Необходимо ввести положительное целое число и больше 0.");
+                Console.WriteLine("Попробуйте еще раз :)");
+            }
 
-                Console.Write("\nВведите загаданное число: ");
-                string UserString = Console.ReadLine();
-                if (UserString == "")
+            if (RangeNumber > 0)
+            {
+                Random rnd = new Random();
+                int RandomNumberForGame = rnd.Next(0, RangeNumber);
+                int count = 0;
+
+                while (true)
                 {
-                    Console.WriteLine("\nОчень жаль, что вы уходите.");
-                    Console.WriteLine($"Загаданное число: {RandomNumberForGame}");
-                    break;
+                    count++;
+
+                    Console.Write("\nВведите загаданное число: ");
+                    string UserString = Console.ReadLine();
+                    if (UserString == "")
+                    {
+                        Console.WriteLine("\nОчень жаль, что вы уходите.");
+                        Console.WriteLine($"Загаданное число: {RandomNumberForGame}");
+                        break;
+                    }
+
+                    int UserNumber = int.Parse(UserString);
+
+                    if (UserNumber < RandomNumberForGame)
+                    {
+                        Console.WriteLine($"Число {UserNumber} меньше загаданного\n");
+                    }
+
+                    else if (UserNumber > RandomNumberForGame)
+                    {
+                        Console.WriteLine($"Число {UserNumber} больше загаданного\n");
+                    }
+
+                    else if (UserNumber == RandomNumberForGame)
+                    {
+                        Console.WriteLine($"\nПоздравляю! Вы угадали число. Количество попыток: {count}");
+                        break;
+                    }
+
                 }
-
-                int UserNumber = int.Parse(UserString);
-
-                if (UserNumber < RandomNumberForGame)
-                {
-                    Console.WriteLine($"Число {UserNumber} меньше загаданного\n");
-                }
-
-                else if (UserNumber > RandomNumberForGame)
-                {
-                    Console.WriteLine($"Число {UserNumber} больше загаданного\n");
-                }
-
-                else if (UserNumber == RandomNumberForGame)
-                {
-                    Console.WriteLine($"\nПоздравляю! Вы угадали число. Количество попыток: {count}");
-                    break;
-                }
-
             }
 
             Console.ReadKey();
